d3fb6a6d08b087b6afd2f83026b4e7ed7e9d65dd
oumaima-kboubi <oumaima.kboubi@insat.u-carthage.tn>
2022-05-31 13:35:12 +0100

    forcast and user auth front

--- forcast-front/src/app/app-routing.module.ts
+++ forcast-front/src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrdersComponent } from './orders/orders.component';
+const routes: Routes = [
+  { path:"", component:HomeComponent },
+  { path:"product-single", component:ProductsingleComponent },
+  { path:"cart", component:CartComponent },
+  { path:"checkout", component:CheckoutComponent },
+  { path:"shop", component:ShopComponent },
+  { path:"dashboard", component:DashboardComponent },
+  { path:"order", component:OrdersComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- forcast-front/src/app/app.module.ts
+++ forcast-front/src/app/app.module.ts
@@ -3,10 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ShopComponent } from './shop/shop.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { AdressComponent } from './adress/adress.component';
+import { AddressComponent } from './address/address.component';
+import { EdditAddressComponent } from './eddit-address/eddit-address.component';
+import { EditAddressComponent } from './edit-address/edit-address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    HeaderComponent,
+    DashboardComponent,
+    ShopComponent,
+    CheckoutComponent,
+    CartComponent,
+    HomeComponent,
+    ProductsingleComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotPasswordComponent,
+    ProfileDetailsComponent,
+    AdressComponent,
+    AddressComponent,
+    EdditAddressComponent,
+    EditAddressComponent
   ],
   imports: [
     BrowserModule,
